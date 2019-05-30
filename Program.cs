e7a4247cc3dc89a5af5537c44520584975f2fb84
Tomas Waisburd <tomaswa@lagash.com>
2019-05-30 17:32:26 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace division_iterativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividendo, divisor;
+
+            pedirParametros(out dividendo, out divisor);
+            
+            try
+            {
+                Console.WriteLine(dividendo + " / " + divisor + " = " + dividir(dividendo, divisor));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static int dividir(int dividendo, int divisor)
+        {
+            if (divisor == 0)
+                throw new DivideByZeroException();
+            int cociente = 0;
+            for(int i = dividendo; i > divisor; i -= divisor, cociente++);
+            return cociente;
+        }
+
+        static void pedirParametros(out int dividendo, out int divisor)
+        {
+            Console.Write("Dividendo: ");
+            dividendo = int.Parse(Console.ReadLine());
+            Console.Write("Divisor: ");
+            divisor = int.Parse(Console.ReadLine());
+        }
+    }
+}
